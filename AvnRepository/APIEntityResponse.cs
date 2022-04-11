7b85b80bb09bf2962395133eb1ffd28e30e5368c
Carl Franklin <carl@franklins.net>
2022-04-10 23:14:56 -0400

    Add project files.

---
+++ AvnRepository/APIEntityResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace AvnRepository;
+public class APIEntityResponse<TEntity> where TEntity : class
+{
+    public bool Success { get; set; }
+    public List<string> ErrorMessages { get; set; } = new List<string>();
+    public TEntity Data { get; set; }
+}
---
+++ AvnRepository/APIListOfEntityResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace AvnRepository;
+public class APIListOfEntityResponse<TEntity> where TEntity : class
+{
+    public bool Success { get; set; }
+    public List<string> ErrorMessages { get; set; } = new List<string>();
+    public IEnumerable<TEntity> Data { get; set; }
+}
